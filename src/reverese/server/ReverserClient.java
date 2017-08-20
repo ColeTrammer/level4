002e99ddefd96564130c8604da2db1304291f159
Cole <league@WTS-IM-17.attlocal.net>
2017-08-20 14:31:59 -0700

    server thing

---
+++ src/reverese/server/ReverserClient.java
@@ -0,0 +1,94 @@
+package reverese.server;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ * A simple Swing-based client for the capitalization server. It has a main
+ * frame window with a text field for entering strings and a textarea to see the
+ * results of capitalizing them.
+ */
+public class ReverserClient {
+
+	private Scanner in;
+	private PrintWriter out;
+	Scanner kb = new Scanner(System.in);
+
+	//Create a member variable for your Socket.
+
+	public boolean update() {
+		out.println(kb.nextLine());
+		String response;
+		try {
+            response = in.nextLine();
+            if (response == null || response.equals("")) {
+                  System.exit(0);
+              }
+        } catch (Exception ex) {
+               response = "Error: " + ex;
+        }
+        System.out.println(response);
+
+		return true;
+	}
+
+	/**
+	 * Implements the connection logic by prompting the end user for the
+	 * server's IP address, connecting, setting up streams, and consuming the
+	 * welcome messages from the server. The Capitalizer protocol says that the
+	 * server sends three lines of text to the client immediately after
+	 * establishing a connection.
+	 */
+	public void connectToServer() throws IOException {
+
+		// Get the server address from a dialog box.
+		String serverAddress = JOptionPane.showInputDialog(null, "Enter IP Address of the Server:",
+				"Welcome to the Capitalization Program", JOptionPane.QUESTION_MESSAGE);
+
+		//Initialize your socket and pass in the serverAddress and port number
+		Socket socket = new Socket(serverAddress, 3000);
+		
+		//initialize your Scanner and pass in your socket's input stream.(Hint: getter)
+		in = new Scanner(socket.getInputStream());
+
+		//initialize your PrintWriter and pass in your socket's output stream.(Hint: getter)
+		out = new PrintWriter(socket.getOutputStream(), true);
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println(in.nextLine() + "\n");
+        }
+
+	}
+
+	
+		
+	
+
+	/**
+	 * Runs the client application.
+	 */
+	public static void main(String[] args) throws Exception {
+		//Instantiate a new ReverserClient();
+		ReverserClient rx = new ReverserClient();
+
+		//call the connectToServer Method
+		rx.connectToServer();
+		
+		//continuously call the client's update method until it is ready to close.
+		while(rx.update()){
+			
+		}
+
+	}
+}
---
+++ src/reverese/server/ReverserServer.java
@@ -0,0 +1,130 @@
+package reverese.server;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * A server program which accepts requests from clients to capitalize strings.
+ * When clients connect, a new thread is started to handle an interactive dialog
+ * in which the client sends in a string and the server thread sends back the
+ * capitalized version of the string.
+ *
+ * The program is runs in an infinite loop, so shutdown in platform dependent.
+ * If you ran it from a console window with the "java" interpreter, Ctrl+C
+ * generally will shut it down.
+ */
+public class ReverserServer extends Thread {
+	/**
+	 * A private thread to handle capitalization requests on a particular
+	 * socket. The client terminates the dialogue by sending a single line
+	 * containing only a period.
+	 * 
+	 * Services this thread's client by first sending the client a welcome
+	 * message then repeatedly reading strings and sending back the reversed
+	 * version of the string.
+	 */
+
+	// **** 1. Create member variables for your socket and client number;
+	Socket socket;
+	int clientNumber = 0;
+
+	/**
+	 * Application method to run the server runs in an infinite loop listening
+	 * on port 9898. When a connection is requested, it spawns a new thread to
+	 * do the servicing and immediately returns to listening. The server keeps a
+	 * unique client number for each client that connects just to show
+	 * interesting logging messages. It is certainly not necessary to do this.
+	 */
+
+	// 2 ****. create a constructor for your ReverserServer that takes in a
+	// Socket an int for your client number
+	ReverserServer(Socket socket, int client) {
+		this.socket = socket;
+		this.clientNumber = client;
+		log("New connection with client# " + clientNumber + " at " + socket);
+	}
+
+	public void run() {
+		try {
+			int number = 0;
+			// Decorate the streams so we can send characters
+			// and not just bytes. Ensure output is flushed
+			// after every newline.
+
+			// 3. Create a Scanner and pass in your socket's inputStream. (hint:
+			// it's a getter)
+			Scanner in = new Scanner(socket.getInputStream());
+
+			// 4. Create a PrintWriter 'out', and pass it your socket's output
+			// stream.
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+			// 5. Use your PrintWriter Send a welcome message to the client.
+			out.println("Hello, you are client #" + clientNumber + ".");
+			out.println("Enter a line with only a period to quit\n");
+
+			while (true) {
+				// 6. read each line using your BufferedReader. This is the user
+				// input
+					String input = in.nextLine();
+					if (input == null || input.equals(".")) {
+						break;
+					}
+					System.out.println("Client " + this.clientNumber + ": " + input);
+					out.println(++number + ". " + input);
+					
+				}
+		} catch (IOException e) {
+			log("Error handling client# " + clientNumber + ": " + e);
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				log("Couldn't close a socket, what's going on?");
+			}
+			log("Connection with client# " + clientNumber + " closed");
+		}
+
+	}
+
+	/**
+	 * Logs a simple message. In this case we just write the message to the
+	 * server applications standard output.
+	 */
+	public static void main(String[] args) throws Exception {
+		System.out.println("The capitalization server is running.");
+
+		// 10. Create a variable to hold a client number
+		int client = 0;
+
+		// 11. Create a SeverSocket on port 9898
+		ServerSocket listener = new ServerSocket(3000);
+		try {
+			while (true) {
+				// 12. call the accept method on your ServerSocket and store it
+				// in a Socket Variable
+				Socket socket = listener.accept();
+
+				// 14. create an instance of a ReverserServer and pass it your
+				// listener
+				// and client number.
+				ReverserServer rc = new ReverserServer(socket, ++client);
+
+				// 15. Start your Reverser.
+				rc.start();
+			}
+		} finally {
+			// 16. Don't forget to close your ServerSocket!
+			listener.close();
+		}
+	}
+
+	private void log(String message) {
+		System.out.println(message);
+	}
+
+}
